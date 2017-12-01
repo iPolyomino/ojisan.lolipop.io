ab51462337abfae400d00aca0a3db3a76c550b73
iPolyomino <dpm26508@kwansei.ac.jp>
2017-12-01 09:22:22 +0900

    made app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
